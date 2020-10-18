6682e573613e3629cdbbb08cf32ec6fadbfc4af5
Will Walker <williamwalker753@gmail.com>
2020-10-17 19:00:49 -0500

    finished styling mod menu, added about section

--- src/components/ModifiersMenu.js
+++ src/components/ModifiersMenu.js
@@ -3,31 +3,52 @@ import store from './store';
 import storeFunctions from './storeFunctions';
 import './ModifiersMenu.css';
 
-let curMenu = '';
+let curMenu = {
+    name: '',
+    clicks: 0,
+};
 export default class ModifiersMenu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-             modObj: {title: '', desc: '', img: '', placeholder: ''},
-             input1: '',
-             input2: '',
-             noinputtext: '',
+            modObj: {title: 'How do I use this?', desc: "Searchlin is an advanced Google search creator. By selecting an option on the left you can add a modifier to your search. You can remove search terms individually or remove all with the arrow circle above. Don't forget about the time search in the bar above", img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/LinkedSite.png?raw=true/', placeholder1: '', input1: 'remove', input2: 'remove', button: 'remove',},
+            input1: '',
+            input2: '',
+            noinputtext: '',
+            safe: false,
+            safeBox: 'far fa-square',
+            newWindow: false,
+            newWindowBox: 'far fa-square',
+            personalizedSearch: false,
+            personalizedSearchBox: 'far fa-square',
+            adTest: false,
+            adTestBox: 'far fa-square',
         };
     }
     handleChange = e => {
         this.setState({ [e.target.name]: e.target.value});
     }
     menuSelect = e => {
-        if(e.target.id === curMenu){
-            document.getElementById('modifiers-menu-form').className = 'hidden';
+        if((e.target.id === curMenu.name)&&(curMenu.clicks === 0)){
+            this.setState({ modObj: store.Default});
+            curMenu.clicks = 1;
         }
         else {
             this.setState({ modObj: store[e.target.id]});
-            curMenu = e.target.id;
-            document.getElementById('modifiers-menu-form').className = '';
+            curMenu.name = e.target.id;
+            curMenu.clicks = 0;
         }
     }
     handleToggle = e => {
+        let name = e.target.id;
+        let nameBox = e.target.id + 'Box';
+        this.setState({ [name]: !this.state.[name] });
+        if(this.state.[name] === true) {
+            this.setState({ [nameBox]: 'far fa-square' });
+        }
+        if(this.state.[name] === false) {
+            this.setState({ [nameBox]: 'far fa-check-square' });
+        }
         let index = store.boolArr.findIndex(element => element.id === e.target.id);
         store.boolArr[index].value = !store.boolArr[index].value;
     }
@@ -40,7 +61,7 @@ export default class ModifiersMenu extends Component {
         }
         else {
             this.setState({ noinputtext: '' });
-            let filteredInput = storeFunctions[curMenu](string1, string2);
+            let filteredInput = storeFunctions[curMenu.name](string1, string2);
             store.modArr.push(filteredInput);
         }
     }
@@ -65,28 +86,32 @@ export default class ModifiersMenu extends Component {
                     <li onClick={this.menuSelect} id='LinkedSite'>Linked Sites</li>
                     <hr/>
                     <form>
-                        <input type='checkbox' id='safe' name='safe' onClick={this.handleToggle}/>
+                        <input type='checkbox' className='checkbox' id='safe' name='safe' onClick={this.handleToggle}/>
+                        <i className={this.state.safeBox}></i>
                         <label htmlFor='safe'>Safe Search</label><br/>
                         <hr/>
-                        <input type='checkbox' id='newWindow' name='newWindow' onClick={this.handleToggle}/>
+                        <input type='checkbox' className='checkbox' id='newWindow' name='newWindow' onClick={this.handleToggle}/>
+                        <i className={this.state.newWindowBox}></i>
                         <label htmlFor='newWindow'>New Window</label><br/>
                         <hr/>
-                        <input type='checkbox' id='personalizedSearch' name='personalizedSearch' onClick={this.handleToggle}/>
+                        <input type='checkbox' className='checkbox' id='personalizedSearch' name='personalizedSearch' onClick={this.handleToggle}/>
+                        <i className={this.state.personalizedSearchBox}></i>
                         <label htmlFor='personalizedSearch'>Personalized Search Off</label><br/>
                         <hr/>
-                        <input type='checkbox' id='adTest' name='adTest' onClick={this.handleToggle}/>
+                        <input type='checkbox' className='checkbox' id='adTest' name='adTest' onClick={this.handleToggle}/>
+                        <i className={this.state.adTestBox}></i>
                         <label htmlFor='adTest'>AdWords DB Disconnect</label><br/>
                     </form>
                 </ul>
-                <div id='modifiers-menu-form' className='hidden'>
+                <div id='modifiers-menu-form'>
                     <h3>{this.state.modObj.title}</h3>
                     <p>{this.state.modObj.desc}</p>
                     {/*<img src={this.state.modObj.img} id='modifiers-menu-form-pic' alt='modifier representation'/> doesn't really look that good with pics*/}
                     <form>
-                        <input type='text'  name='input1' onChange={this.handleChange} placeholder={this.state.modObj.placeholder1}/>
+                        <input type='text'  name='input1' onChange={this.handleChange} placeholder={this.state.modObj.placeholder1} className={this.state.modObj.input1}/>
                         <p className={this.state.modObj.input2}>{this.state.modObj.joiner}</p>
                         <input type='text'  name='input2' onChange={this.handleChange} placeholder={this.state.modObj.placeholder2} className={this.state.modObj.input2}/>
-                        <button type='submit' onClick={this.submit}><i className="fas fa-plus-circle"></i></button>
+                        <button type='submit' className={this.state.modObj.button} onClick={this.submit}><i className="fas fa-plus"></i></button>
                     </form>
                 <p id='no-input-text'>{this.state.noinputtext}</p>
                 </div>
--- src/components/store.js
+++ src/components/store.js
@@ -1,12 +1,13 @@
 const Obj = function() {
-    const GeneralMatch = {title: 'General Match', desc: 'Find generic matches with these terms', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/GeneralMatch.png?raw=true/', placeholder1: 'shoes', input2: 'remove'};
-    const ExactMatch = {title: 'Exact Match', desc: 'Only verbatim results', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/ExactMatch.png?raw=true/', placeholder1: 'cheesy garlic bread', input2: 'remove'};
-    const OR = {title: 'Or', desc: 'Find results for either', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/Or.png?raw=true/', placeholder1: 'Gates', joiner: 'OR', placeholder2: 'Jobs',input2: ''};
-    const DontInclude = {title: "Don't Include", desc: 'Remove results with these words', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/DontInclude.png?raw=true/', placeholder1: 'Microsoft', input2: 'remove'};
-    const SiteSearch = {title: 'Site Search', desc: 'Only results from this site', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/SiteSearch.png?raw=true/', placeholder1: 'amazon.com', input2: 'remove'};
-    const FileType = {title: 'File Type', desc: 'Results with this file type', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/FileType.png?raw=true/', placeholder1: 'pdf', input2: 'remove'};
-    const RelatedSite = {title: 'Related Sites', desc: 'Results from sites in same category', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/RelatedSite.png?raw=true/', placeholder1: 'amazon.com', input2: 'remove'};
-    const LinkedSite = {title: 'Linked Sites', desc: 'Results from sites linked to this one', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/LinkedSite.png?raw=true/', placeholder1: 'amazon.com', input2: 'remove'};
+    const Default = {title: 'How do I use this?', desc: "Searchlin is an advanced Google search creator. By selecting an option on the left you can add a modifier to your search. You can remove search terms indivually or remove all with the arrow circle above. Don't forget about the time search in the bar above", img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/LinkedSite.png?raw=true/', placeholder1: '', input1: 'remove', input2: 'remove', button: 'remove',};
+    const GeneralMatch = {title: 'General Match', desc: 'Find generic matches', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/GeneralMatch.png?raw=true/', placeholder1: 'shoes', input1: '', input2: 'remove'};
+    const ExactMatch = {title: 'Exact Match', desc: 'Only verbatim results', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/ExactMatch.png?raw=true/', placeholder1: 'cheesy garlic bread', input1: '', input2: 'remove'};
+    const OR = {title: 'Or', desc: 'Find results for either', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/Or.png?raw=true/', placeholder1: 'Gates', joiner: 'OR', placeholder2: 'Jobs',input1: '', input2: ''};
+    const DontInclude = {title: "Don't Include", desc: 'Remove results with these', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/DontInclude.png?raw=true/', placeholder1: 'Microsoft', input1: '', input2: 'remove'};
+    const SiteSearch = {title: 'Site Search', desc: 'Only results from this site', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/SiteSearch.png?raw=true/', placeholder1: 'amazon.com', input1: '', input2: 'remove'};
+    const FileType = {title: 'File Type', desc: 'Results with this file type', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/FileType.png?raw=true/', placeholder1: 'pdf', input1: '', input2: 'remove'};
+    const RelatedSite = {title: 'Related Sites', desc: 'Find sites in same category', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/RelatedSite.png?raw=true/', placeholder1: 'amazon.com', input1: '', input2: 'remove'};
+    const LinkedSite = {title: 'Linked Sites', desc: 'Only sites linked to this one', img: 'https://github.com/willwalker753/searchlin/blob/master/src/pictures/LinkedSite.png?raw=true/', placeholder1: 'amazon.com', input1: '', input2: 'remove'};
 
     let boolArr = [
         {name:'Safe Search', id: 'safe', value: false, url: 'safe=active'},
@@ -17,6 +18,7 @@ const Obj = function() {
     let modArr = [];
 
     return {
+        Default: Default,
         GeneralMatch: GeneralMatch,
         ExactMatch: ExactMatch,
         OR: OR,
